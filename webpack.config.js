c83e07ae4d72c2a7db36e44928ccb585a3e2a322
0xNordian <85548493+0xNordian@users.noreply.github.com>
2023-08-23 23:51:45 +0200

    fourth attempt bug images using webpack

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    // ... other webpack configuration ...
+
+    plugins: [
+        // ... other plugins ...
+        new CopyWebpackPlugin([
+            { from: 'src/data', to: 'data' }, // Copy the data directory
+            { from: 'src/assets', to: 'assets' }, // Copy the assets directory
+        ]),
+    ],
+};
